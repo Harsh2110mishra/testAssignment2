b241f2ee14f6923500c7f0aa45c82d4d7dc67c31
Harsh Mishra <50140154+Harsh2110mishra@users.noreply.github.com>
2023-03-07 16:29:41 +0530

    Create cart.js

---
+++ cart.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function Cart(props) {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        {props.name}
+      </Text>
+     <Text style={styles.paragraph}>
+        {props.quanity} x {props.price}
+      </Text>
+       <Text style={styles.paragraph}>
+        {total}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  logo: {
+    height: 128,
+    width: 128,
+  }
+});
